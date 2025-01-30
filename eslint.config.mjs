6cf0c13ff02c098ac07e74d08ebf443d1d6b3667
shoma-fujita <syouteni123@gmail.com>
2025-01-30 22:00:53 +0900

    ESLint Stylistic の適応と自動保存対応

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,13 @@
-// @ts-check
+import stylistic from '@stylistic/eslint-plugin'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
+  {
+    files: ['**/*.vue', '**/*.ts'],
+  },
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: 'single',
+    semi: false,
+  }),
 )
